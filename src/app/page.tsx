d71a82bf677e8914312c351c137de35339d316d7
RomanFronrEnd <romariotraveler@gmail.com>
2026-05-02 18:23:26 +0300

    const dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ export const metadata = {
 	description: 'Find the best real estate properties...',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
 	let properties;
 
